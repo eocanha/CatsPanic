3a2a4a261b339c703394c992d110da1af3f92a2b
Enrique Ocaña González <eocanha@igalia.com>
2023-01-14 06:44:21 +0100

    Also export stages.conf

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,5 +2,5 @@
 cd $(dirname ${BASH_SOURCE[0]})/..
 rm -rf cats-panic-linux-amd64 cats-panic-linux-amd64.tgz
 mv linux-amd64 cats-panic-linux-amd64
-cp -a pics cats-panic-linux-amd64/
+cp -a pics stages.conf cats-panic-linux-amd64/
 tar zcvf cats-panic-linux-amd64.tgz cats-panic-linux-amd64
